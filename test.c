43d63cf448bcb0f28359125b03ab80dd02ed42c8
Khoi <nguyendkhoi25@gmail.com>
2026-02-03 10:41:17 +0100

    Change tache4

--- test.c
+++ test.c
@@ -1,7 +1,9 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 #include "image.h"
 #include "contour.h"
+#include "sequence_point.h"
 
 int main(int argc, char **argv) {
 
@@ -18,11 +20,37 @@ int main(int argc, char **argv) {
     int h = hauteur_image(I);
     int nb_segments = contour.taille - 1;
     
-    printf("Fichier: %s\n", argv[1]);
-    printf("Dimensions: %d x %d\n", w, h);
-    printf("Nombre de segments: %d\n", nb_segments);
-    printf("\n");
+    string fichier_nom = argv[1];
+    size_t pos = fichier_nom.find('.pbm');
+    string word = fichier_nom.substr(0, pos).append(".eps");
     
+    File *f = fopen(word, "w");
+
+    fprintf(f, "%%!PS-Adobe-3.0 EPSF-3.0\n", "%%%%BoundingBox 0 0 %d %d\n", w, h);
+    fprintf(f, "/l {lineto} def\n", "/m {moveto} def\n", "/s {stroke} def\n");
+
+    Cellule_Liste_Point *LP = contour.first;
+    Point P = LP->data;
+
+    fprintf(f, "%d %d m\n", P.x, P.y);
+
+    Cellule_Liste_Point *suivLP= LP->suiv;
+
+    while (suivLP != NULL) {
+        Point suivPoint = suivLP->data;
+        fprintf(f, "%d %d l\n", suivPoint.x, suivPoint.y);
+		suivLP = suivLP->suiv;
+    }
+
+    fprintf(f, "%d %d l\n", P.x, P.y);
+
+    string type;
+    if (argv[2] == 1) type = "stroke";
+    else type = "fill";
+
+    fprintf(f, "0 setlinewith %s\n", type);
+    fprintf(f, "showpage");
+
     contour = supprimer_liste_Point(contour);
     supprimer_image(&I);
     
