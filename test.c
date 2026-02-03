f7531c6dba50bf761918ab08e3231342df31f19e
Khoi <nguyendkhoi25@gmail.com>
2026-02-03 15:15:26 +0100

    Change test

--- test.c
+++ test.c
@@ -6,50 +6,61 @@
 #include "sequence_point.h"
 
 int main(int argc, char **argv) {
+    if (argc < 3) {
+        printf("Usage : %s <fichier_image.pbm> <1 (stroke) | 2 (fill)>\n", argv[0]);
+        return 1;
+    }
+    
+    char output_filename[256];
+    strcpy(output_filename, argv[1]);
+    char *dot = strrchr(output_filename, '.');
+    strcat(output_filename, ".eps");
 
-    if (argc < 2) {
-        printf("Usage : %s <fichier_image.pbm>\n", argv[0]);
+    FILE *f = fopen(output_filename, "w");
+    if (f == NULL) {
+        printf("Erreur : Impossible de creer le fichier %s\n", output_filename);
         return 1;
     }
 
     Image I = lire_fichier_image(argv[1]); 
-
     Liste_Point contour = calculer_contour(I);
 
     int w = largeur_image(I);
     int h = hauteur_image(I);
-    int nb_segments = contour.taille - 1;
-    
-    string fichier_nom = argv[1];
-    size_t pos = fichier_nom.find('.pbm');
-    string word = fichier_nom.substr(0, pos).append(".eps");
-    
-    File *f = fopen(word, "w");
 
-    fprintf(f, "%%!PS-Adobe-3.0 EPSF-3.0\n", "%%%%BoundingBox 0 0 %d %d\n", w, h);
-    fprintf(f, "/l {lineto} def\n", "/m {moveto} def\n", "/s {stroke} def\n");
+    fprintf(f, "%%!PS-Adobe-3.0 EPSF-3.0\n");
+    fprintf(f, "%%%%BoundingBox 0 0 %d %d\n", w, h);
+
+    fprintf(f, "/l {lineto} def\n");
+    fprintf(f, "/m {moveto} def\n");
+    fprintf(f, "/s {stroke} def\n");
 
     Cellule_Liste_Point *LP = contour.first;
-    Point P = LP->data;
 
-    fprintf(f, "%d %d m\n", P.x, P.y);
+    Point P = LP->data;
+    fprintf(f, "%f %f m\n", P.x, P.y);
 
     Cellule_Liste_Point *suivLP= LP->suiv;
-
     while (suivLP != NULL) {
         Point suivPoint = suivLP->data;
-        fprintf(f, "%d %d l\n", suivPoint.x, suivPoint.y);
+        fprintf(f, "%f %f l\n", suivPoint.x, suivPoint.y);
 		suivLP = suivLP->suiv;
     }
 
-    fprintf(f, "%d %d l\n", P.x, P.y);
+    fprintf(f, "%f %f l\n", P.x, P.y);
 
-    string type;
-    if (argv[2] == 1) type = "stroke";
-    else type = "fill";
+    fprintf(f, "0 setlinewidth\n");
 
-    fprintf(f, "0 setlinewith %s\n", type);
+    if (strcmp(argv[2], "1") == 0) {
+        fprintf(f, "stroke\n");
+    }
+    else {
+        fprintf(f, "fill\n");
+    }
+
     fprintf(f, "showpage");
+
+    fclose(f);
 
     contour = supprimer_liste_Point(contour);
     supprimer_image(&I);
